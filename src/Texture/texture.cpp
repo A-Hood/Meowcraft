788784d0e9c2bbc151cf3729aba2b77b14e21e5c
A-Hood <123602100+A-Hood@users.noreply.github.com>
2025-06-24 09:11:47 +0100

    Started working on texture class
    Loads only one image right now, will add texture atlas and allow each block to pick from the atlas depending on enum type.

--- src/Texture/texture.cpp
+++ src/Texture/texture.cpp
@@ -2,13 +2,36 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+#include <iostream>
+
+#define STB_IMAGE_IMPLEMENTATION
 #include <stb_image/stb_image.h> // read image
 
+#include "textureatlas.h"
 
-
-Texture::Texture(int minX, int maxX, int minY, int maxY) {
+Texture::Texture(std::string fileName) {
     glGenTextures(1, &mTex);
     glBindTexture(GL_TEXTURE_2D, mTex);
-    //glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+
+// load and generate the texture
+    stbi_set_flip_vertically_on_load(true);
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("../../src/Assets/Textures/dirt.png", &width, &height, &nrChannels, 0);
+    if (data)
+    {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+    }
+    else
+    {
+        std::cout << "Failed to load texture" << std::endl;
+    }
+    stbi_image_free(data);
+
     glGenerateMipmap(GL_TEXTURE_2D);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
+                    GL_NEAREST_MIPMAP_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 1);
 }
--- src/Texture/texture.h
+++ src/Texture/texture.h
@@ -1,10 +1,21 @@
 #ifndef TEXTURE_H
 #define TEXTURE_H
 
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <string>
+
+class TextureAtlas;
+
 class Texture {
 public:
     Texture() = default;
-    Texture(int minX, int maxX, int minY, int maxY);
+    Texture(std::string fileName);
+
+    void BindTexture() {
+        glBindTexture(GL_TEXTURE_2D, mTex);
+    }
 
 private:
     unsigned int mTex;
